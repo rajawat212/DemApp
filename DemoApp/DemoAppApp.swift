565cf1d1ce627e1920d000d222cfa9149ddfbcca
Rajesh Singh <rajrajawat111@gmail.com>
2022-12-22 20:19:49 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by apple on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
